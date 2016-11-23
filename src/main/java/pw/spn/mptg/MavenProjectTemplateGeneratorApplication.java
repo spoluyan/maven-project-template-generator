456f0a7757300f64f7bb64453e569de2786d2cd2
Sergey Poluyan <spoluyan@gmail.com>
2016-11-23 10:54:45 +0300

    Initial structure

---
+++ src/main/java/pw/spn/mptg/MavenProjectTemplateGeneratorApplication.java
@@ -0,0 +1,12 @@
+package pw.spn.mptg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenProjectTemplateGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenProjectTemplateGeneratorApplication.class, args);
+    }
+}
